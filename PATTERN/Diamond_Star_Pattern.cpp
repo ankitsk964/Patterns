8c5c89cf40e807af5c85cecb2caf2d91ef5849d2
Ankit Kumar <73459601+ankitsk964@users.noreply.github.com>
2026-01-13 11:49:05 +0530

    Add comment block for diamond star pattern example
    Added a comment block to illustrate the diamond star pattern output.

--- PATTERN/Diamond_Star_Pattern.cpp
+++ PATTERN/Diamond_Star_Pattern.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
 int main() {
   int size = 5;
   // upside pyramid
@@ -28,4 +42,5 @@ int main() {
     cout << "\n";
   }
   return 0;
+
 }
